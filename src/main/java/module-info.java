ee3ee9205f96c26ded34dcce93b423eab0962f78
ariel <stevekamga18@gmail.com>
2024-07-18 10:42:13 +0100

    init

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,13 +2,10 @@ module com.example.demo {
     requires javafx.controls;
     requires javafx.fxml;
 
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires net.synedra.validatorfx;
-    requires org.kordamp.bootstrapfx.core;
-    requires mongo.java.driver;
     requires java.sql;
 
     opens com.example.demo to javafx.fxml;
     exports com.example.demo;
+    exports com.example.demo.controller;
+    opens com.example.demo.controller to javafx.fxml;
 }
